aca442e40f44f5c1e6394ee21486a07a03776068
mhamzaaamir960 <mhamzaaamir960.gmail.com>
2024-02-09 23:53:53 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,32 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import NavBar from '@/components/NavBar'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Muhammad Hamza | Portfolio',
-  description: '',
-}
+  title: "Muhammad Hamza | Portfolio",
+  description: "",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className } `}>
-         <NavBar/>
+      <body className={`${inter.className} `}>
+        <NavBar />
         {children}
       </body>
     </html>
-  )
+  );
 }
 
-{/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+{
+  /* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 -&gt;
-</span> */}
+</span> */
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Link from 'next/link';
-import { Switch } from "@/components/ui/switch"
-import { GaugeCircle } from 'lucide-react';
-import { Montserrat } from 'next/font/google';
-import Skills from './Skills';
+import React from "react";
+import Link from "next/link";
+import { Switch } from "@/components/ui/switch";
+import { GaugeCircle } from "lucide-react";
+import { Montserrat } from "next/font/google";
+import Skills from "./Skills";
 
 const mont = Montserrat({
   weight: "700",
@@ -13,34 +13,52 @@ const mont = Montserrat({
 
 function NavBar() {
   return (
-    <nav className='h-[50px] w-full bg-white z-10 shadow-xl flex items-center  justify-between md:justify-normal fixed top-0  '>
+    <nav className="h-[50px] w-full bg-white z-10 shadow-xl flex items-center  justify-between md:justify-normal fixed top-0  ">
+      <span className="flex cursor-pointer gap-x-2 md:w-[10%] ml-2 sm:ml-4 mr-4 shrink-0">
+        <GaugeCircle className={`shrink-0 animate-spin  rotate-90  `} />
+        <span
+          className={`font-medium text-md sm:text-xl drop-shadow-xl hidden sm:block ${mont.className} `}
+        >
+          Portfolio
+        </span>
+      </span>
 
-     <span className='flex cursor-pointer gap-x-2 md:w-[10%] ml-2 sm:ml-4 mr-4 shrink-0'><GaugeCircle className={`shrink-0 animate-spin  rotate-90  `}/>
-     <span className={`font-medium text-md sm:text-xl drop-shadow-xl hidden sm:block ${mont.className} `}>Portfolio</span>
-     </span>
+      <span className="hidden md:flex justify-center md:text-xl lg:text-2xl text-black font-light italic drop-shadow-lg animate-bounce  transition-shadow w-[50%] ">
+        Available for freelance
+      </span>
 
-     <span className='hidden md:flex justify-center md:text-xl lg:text-2xl text-black font-light italic drop-shadow-lg animate-bounce  transition-shadow w-[50%] '>Available for freelance</span>
-     
-     <div  className='w-[30%] flex justify-end'>
-     <ul className='hidden md:block'>
-      <li className='flex gap-x-5 lg:gap-x-10'>
-        <a href="#home">
-        <button className=' text-black lg:text-lg font-sm hover:text-indigo-500 delay-75 duration-300'>Home</button></a>
-        <a href="#projects">
-        <button className=' text-black lg:text-lg font-sm hover:text-indigo-500 delay-75 duration-300 '>Projects</button></a>
-        <a href="#skills"  >
-        <button className=' text-black  lg:text-lg font-sm hover:text-indigo-500 delay-75 duration-300 '>Skills</button></a>
-        <a href="#skills">
-        <button className=' text-black lg:text-lg font-sm hover:text-indigo-500 delay-75 duration-300'>About</button></a>
-      </li>
-     </ul>
-     <span className='flex md:hidden text-2xl mx-6 w-[10%] shrink-0 '>&#9776;</span>
-     </div>
-     <Switch/>
+      <div className="w-[30%] flex justify-end">
+        <ul className="hidden md:block">
+          <li className="flex gap-x-5 lg:gap-x-10">
+            <a href="#home">
+              <button className=" text-black lg:text-lg font-sm hover:text-indigo-500 delay-75 duration-300">
+                Home
+              </button>
+            </a>
+            <a href="#projects">
+              <button className=" text-black lg:text-lg font-sm hover:text-indigo-500 delay-75 duration-300 ">
+                Projects
+              </button>
+            </a>
+            <a href="#skills">
+              <button className=" text-black  lg:text-lg font-sm hover:text-indigo-500 delay-75 duration-300 ">
+                Skills
+              </button>
+            </a>
+            <a href="#skills">
+              <button className=" text-black lg:text-lg font-sm hover:text-indigo-500 delay-75 duration-300">
+                About
+              </button>
+            </a>
+          </li>
+        </ul>
+        <span className="flex md:hidden text-2xl mx-6 w-[10%] shrink-0 ">
+          &#9776;
+        </span>
+      </div>
+      <Switch />
     </nav>
-  )
+  );
 }
 
 export default NavBar;
-
-
